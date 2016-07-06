6a98465c94c6021106c121bd0828f89e042b89a9
Benjamin Martin <benjamin.martin@workplacesystems.com>
2016-07-06 14:27:55 +0100

    Sketched out rough idea

--- lib/fitz_client.ex
+++ lib/fitz_client.ex
@@ -8,31 +8,34 @@ defmodule FitzClient do
   #end
 
   def main(args) do
-    args |> parse_args |> do_process
+    args |> parse_args |> do_process |> do_request
   end
 
   def parse_args(args) do
     {options, _, _} = OptionParser.parse(args,
-      switches: [user: :string, page: :integer, page_index: :integer]
+      switches: [user: :string, page: :integer, page_size: :integer]
     )
-
-    options
-
-    #case options do
-    #  {[user: '', page: 1, page_size: 100], _, _} -> [user: user, page: page, page_size: page_size]
-    #  {[help: true], _, _} -> :help
-    #  _ -> :help
-    #end
+    case options do
+      [user: user, page: page, page_size: page_size] -> [user: user, page: page, page_size: page_size]
+      _ -> :help
+    end
   end
 
   def do_process([user: user, page: page, page_size: page_size]) do
     IO.puts "Use: #{page} -- #{page} -- #{page_size}"
+     [user, page, page_size]
+  end
+
+  def do_request([user, page, page_size]) do
+    HTTPoison.start
+	res = HTTPoison.get! "http://fizzbuzz.127-0-0-1.org.uk:3000/fizz?user_id=#{user}&page=#{page}&page_size=#{page_size}"
+    IO.puts "REQ: User: #{user} Page: #{page} Size: #{page_size} -- #{res.body}"
   end
 
   def do_process(:help) do
     IO.puts """
       Usage:
-      ./fitz_client --page [page number]
+      ./fitz_client --user [user id] --page [page number] --page-size [number of results per page]
 
       Options:
       --user      Provide a user id
